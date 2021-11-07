6aaff5a0de39c533aa7c65fa33036c929a989d11
sam <sammtcbn@gmail.com>
2021-11-07 21:45:20 +0800

    curl download with -L parameter

--- util/download-msys2-installer.bat
+++ util/download-msys2-installer.bat
@@ -2,7 +2,7 @@
 set dlver=20210725
 set dlfile=msys2-x86_64-%dlver%.exe
 set dlurl=https://github.com/msys2/msys2-installer/releases/download/2021-07-25/%dlfile%
-curl --fail --output %dlfile% %dlurl%
+curl --fail --output %dlfile% -L %dlurl%
 if %errorlevel% neq 0 (
     echo Fail to download %dlfile%
     goto End
