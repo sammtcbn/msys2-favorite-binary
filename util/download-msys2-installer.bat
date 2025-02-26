a75163af432eb89e212dec20cabef1404ec2c158
sammtcbn <sammtcbn@gmail.com>
2025-02-26 18:41:51 +0800

    upgrade binary from msys2-x86_64-20250221.exe

--- util/download-msys2-installer.bat
+++ util/download-msys2-installer.bat
@@ -1,7 +1,7 @@
 @echo off
-set dlver=20230127
+set dlver=20250221
 set dlfile=msys2-x86_64-%dlver%.exe
-set dlurl=https://github.com/msys2/msys2-installer/releases/download/2023-01-27/%dlfile%
+set dlurl=https://github.com/msys2/msys2-installer/releases/download/2025-02-21/%dlfile%
 curl --fail --output %dlfile% -L %dlurl%
 if %errorlevel% neq 0 (
     echo Fail to download %dlfile%
