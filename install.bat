6cbba0299eb1d453ee4f199c8fd5747d75251a3e
sammtcbn <sammtcbn@gmail.com>
2026-09-18 13:35:01 +0800

    change variable for install destination

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 @set currdir=%~dp0
-@set targetpath=C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps
+@set targetpath=%LocalAppData%\Microsoft\WindowsApps
 @del /f /q %targetpath%\msys-*.dll
 @xcopy /Y %currdir%\bin\*.dll %targetpath%
 @xcopy /Y %currdir%\bin\*.exe %targetpath%
